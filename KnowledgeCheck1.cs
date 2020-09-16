ef5ca7661460ba2cfcb84191d91273b9bfeedf33
Luis <m.luishernandez@outlook.com>
2020-09-16 12:41:12 -0400

    Finished code. Have to inspect to make shorter.

---
+++ KnowledgeCheck1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace KnowledgeCheck1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter grade point average");
+            double userGrade = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter test score");
+            double userScore = Convert.ToDouble(Console.ReadLine());
+
+            if (userGrade >= 3.0 && userScore >= 60)
+            {
+                Console.WriteLine("Accept");
+            }
+
+            else if (userGrade <= 3.0 && userScore >= 80)
+            {
+                Console.WriteLine("Accept");
+            }
+
+            else if (userGrade <= 3.0 || userScore <= 60)
+            {
+                Console.WriteLine("Reject");
+            }
+
+            else if (userGrade <= 3.0 && userScore <= 80)
+
+            {
+                Console.WriteLine("Reject");
+            }
+
+        
+        }
+    }
+}
